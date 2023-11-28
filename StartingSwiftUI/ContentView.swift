3dec427805ac420fe2a51a8d0a1409c09f1c68b7
David Castillo Fuentes <dcf82@hotmail.com>
2023-11-28 05:16:46 -0600

    Initial Commit

---
+++ StartingSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StartingSwiftUI
+//
+//  Created by David Castillo Fuentes on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StartingSwiftUI/StartingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartingSwiftUIApp.swift
+//  StartingSwiftUI
+//
+//  Created by David Castillo Fuentes on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct StartingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
